8c65a2a69396da1cd3adf27fe2e80cdaa5c84df8
Filipe Lima de Souza <filipe.lima@rydoo.com>
2020-04-13 18:37:01 +0200

    it's alive

--- src/sensors/Main.cpp
+++ src/sensors/Main.cpp
@@ -1,42 +1,53 @@
-#include<stdio.h>
-#include<string.h>	//strlen
-#include<sys/socket.h>
-#include<arpa/inet.h>	//inet_addr
+#include <stdio.h>
+#include <string.h> //strlen
+#include <sys/socket.h>
+#include <arpa/inet.h> //inet_addr
 
-int main(int argc , char *argv[])
+int main(int argc, char *argv[])
 {
 	int socket_desc;
 	struct sockaddr_in server;
-	char *message;
-	
+
 	//Create socket
-	socket_desc = socket(AF_INET , SOCK_STREAM, 0);
+	socket_desc = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (socket_desc == -1)
 	{
-		printf("Could not create socket");
+		printf("Could not create socket\n");
+		return -1;
 	}
+	else
+	{
+		printf("Socket Created\n");
+	}
+	
+	memset(&server, 0, sizeof(server));
 
-	server.sin_addr.s_addr = inet_addr("localhost");
 	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr("172.16.238.10");
 	server.sin_port = htons(15002);
 
 	//Connect to remote server
-	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
+	int conn = connect(socket_desc, (struct sockaddr *)&server, sizeof(server));
+	if (conn < 0)
 	{
-		puts("connect error");
-		return 1;
+		printf("Connection failed! Error %d\n", conn);
+		return -1;
 	}
-	
+	else
+	{
+		printf("Connect successfully done.\n");
+	}
+
 	puts("Connected\n");
-	
+
 	//Send some data
-	message = "GET / HTTP/1.1\r\n\r\n";
-	if( send(socket_desc , message , strlen(message) , 0) < 0)
+	char const *message = "GET / HTTP/1.1\r\n\r\n";
+	if (send(socket_desc, message, strlen(message), 0) < 0)
 	{
 		puts("Send failed");
 		return 1;
 	}
 	puts("Data Send\n");
-	
+
 	return 0;
 }
