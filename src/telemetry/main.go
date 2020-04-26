dacbad6fdc58d8924492855cb85806346c2d2560
Filipe Lima de Souza <filipe.lima@rydoo.com>
2020-04-26 17:31:23 +0200

    removing semi comma

--- src/telemetry/main.go
+++ src/telemetry/main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "spacef/telemetry/telemetry_server"
+	"fmt"
+	"os"
+	"spacef/telemetry/telemetry_server"
 )
 
 func main() {
-  fmt.Println("Start server...")
+	fmt.Println("Start server...")
 
-  server, err := telemetry_server.Connect(15002)
-  checkError(err)
+	server, err := telemetry_server.Connect(15002)
+	checkError(err)
 
-  fmt.Println("Server started...")
+	fmt.Println("Server started...")
 
-  handler := telemetry_server.CreateHandler();
-  
-  fmt.Println("Handling connections...")
+	handler := telemetry_server.CreateHandler()
 
-  server.HandleConnections(handler);
+	fmt.Println("Handling connections...")
+
+	server.HandleConnections(handler)
 }
 
 func checkError(err error) {
-  if err != nil {
-      fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-      os.Exit(1)
-  }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
 }
